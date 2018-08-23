1d58d8f1ba5c47e449cdc39001813d7ab9d05555
AinulHamdani <hamdani.ainul@gmail.com>
2018-08-23 11:19:12 +0800

    AH | Add main API
    Signed-off-by: AinulHamdani <hamdani.ainul@gmail.com>

---
+++ index.php
@@ -0,0 +1,6 @@
+<?php
+
+define('BASE_URL', "http://localhost/atma");
+
+require '/vendor/autoload.php';
+require_once 'public/index.php';
---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+
+require_once 'config/database.php';
+
+use \Psr\Http\Message\ServerRequestInterface as Request;
+use \Psr\Http\Message\ResponseInterface as Response;
+
+
+$app = new \Slim\App(["settings" => $config]);
+
+$app->get('/', function (Request $request, Response $response, array $args) {
+    $response->getBody()->write("Atma Project API V1");
+
+    return $response;
+});
+
+$app->run();
