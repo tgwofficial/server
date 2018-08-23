ac4b8fb32cbaaf81d242c0a58cea17a385538fba
AinulHamdani <hamdani.ainul@gmail.com>
2018-08-23 12:56:06 +0800

    AH | add DB connector and view template
    Signed-off-by: AinulHamdani <hamdani.ainul@gmail.com>

--- public/index.php
+++ public/index.php
@@ -14,4 +14,24 @@ $app->get('/', function (Request $request, Response $response, array $args) {
     return $response;
 });
 
+$container = $app->getContainer();
+
+$container['db'] = function ($c) {
+    $db = $c['settings']['db'];
+    $pdo = new PDO("mysql:host=" . $db['host'] . ";dbname=" . $db['dbname'],
+        $db['user'], $db['pass']);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    return $pdo;
+};
+
+$container['view'] = new \Slim\Views\PhpRenderer("./templates/");
+
+$container['logger'] = function($c) {
+    $logger = new \Monolog\Logger('my_logger');
+    $file_handler = new \Monolog\Handler\StreamHandler("./logs/app.log");
+    $logger->pushHandler($file_handler);
+    return $logger;
+};
+
 $app->run();
