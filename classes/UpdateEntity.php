e580afea00f3aaadcf4b787daa1552a649f9401d
AinulHamdani <hamdani.ainul@gmail.com>
2018-08-23 19:42:01 +0800

    AH | add updates table mapper
    Signed-off-by: AinulHamdani <hamdani.ainul@gmail.com>

---
+++ classes/UpdateEntity.php
@@ -0,0 +1,51 @@
+<?php
+
+class UpdateEntity
+{
+    protected $id;
+    protected $update_id;
+    protected $form_name;
+    protected $data;
+    protected $user_id;
+    protected $server_timestamp;
+
+	public function __construct(array $data) {
+        if(isset($data['id'])) {
+            $this->id = $data['id'];
+        }
+        $this->update_id = $data['update_id'];
+        $this->form_name = $data['form_name'];
+        $this->data = $data['data'];
+        $this->user_id = $data['user_id'];
+        if(isset($data['server_timestamp'])) {
+            $this->server_timestamp = $data['server_timestamp'];
+        }else{
+            $this->server_timestamp = date("Y-m-d H:i:s");
+        }
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    public function getUpdateId() {
+        return $this->update_id;
+    }
+
+    public function getFormName() {
+        return $this->form_name;
+    }
+
+    public function getData() {
+        return $this->data;
+    }
+
+    public function getUserId() {
+        return $this->user_id;
+    }
+
+    public function getServerTimestamp() {
+        return $this->server_timestamp;
+    }
+
+}
---
+++ classes/UpdateMapper.php
@@ -0,0 +1,59 @@
+<?php
+
+class UpdateMapper extends Mapper
+{
+	public function getUpdates($update_id) {
+        $sql = "SELECT *
+            from updates WHERE update_id > $update_id";
+        $stmt = $this->db->query($sql);
+
+        $results = [];
+        while($row = $stmt->fetch()) {
+            $results[] = new UpdateEntity($row);
+        }
+        return $results;
+    }
+
+	public function getBatchUpdates($update_id,$batch) {
+        $sql = "SELECT *
+            from updates WHERE update_id > $update_id LIMIT $batch";
+        $stmt = $this->db->query($sql);
+
+        $results = [];
+        while($row = $stmt->fetch()) {
+            $results[] = new UpdateEntity($row);
+        }
+        return $results;
+    }
+
+    public function getUpdateById($id) {
+        $sql = "SELECT *
+            from updates where update_id = :update_id";
+        $stmt = $this->db->prepare($sql);
+        $stmt->execute(["update_id" => $id]);
+
+        return new UpdateEntity($stmt->fetch());
+    }
+
+    public function save(UpdateEntity $update) {
+        $sql = "INSERT INTO updates
+            (update_id, form_name, data, user_id) values
+            (:update_id, :form_name, :data, :user_id)";
+
+        $stmt = $this->db->prepare($sql);
+        $result = $stmt->execute([
+            "update_id" => $update->getUpdateId(),
+			"form_name" => $update->getFormName(),
+			"data" => json_encode($update->getData()),
+			"user_id" => $update->getUserId(),
+        ]);
+
+        if(!$result) {
+            throw new Exception("could not save record");
+        }
+    }
+
+	public function saveBatch(UpdateEntity $updates) {
+
+	}
+}
